705761978b2f959064ce4b43c97158adfb42df1e
Maxime PYZ <maxime.pyz@etu.umontpellier.fr>
2017-03-15 10:30:26 +0100

    Déplacement de la classe time dans le package database et implémentation de la classe Time

---
+++ src/fr/jgetmove/jgetmove/database/Time.java
@@ -0,0 +1,69 @@
+package fr.jgetmove.jgetmove.database;
+
+import java.util.HashMap;
+import fr.jgetmove.jgetmove.database.*;
+
+
+/**
+ * Objet Time contenant la liste des clusters pour un temps donné
+ */
+public class Time {
+	private int id;
+	/**
+	 * Hashmap contenant la liste des clusters idCluster=>Cluster
+	 */
+	private HashMap<Integer,Cluster> clusters;
+
+	/**
+	 * 
+	 * @param id identifiant du temps
+	 */
+	Time(int id){
+		this.id = id;
+		clusters = new HashMap<>();
+	}
+	
+	/**
+	 * 
+	 * @param cluster ajoute une cluster à la liste des clusters
+	 */
+	void add(Cluster cluster) {
+        this.clusters.put(cluster.getId(), cluster);
+    }
+	
+	/**
+	 * 
+	 * @return id du temps
+	 */
+	int getId() {
+		return id;
+	}
+	/**
+	 * 
+	 * @param id du temps
+	 */
+	void setId(int id) {
+		this.id = id;
+	}
+	/**
+	 * 
+	 * @return ensemble des cluster
+	 */
+	HashMap<Integer, Cluster> getClusters() {
+		return clusters;
+	}
+	
+	/**
+	 * 
+	 * @param clusters assigne un tableau de cluster à la hashmap
+	 */
+	void setClusters(HashMap<Integer, Cluster> clusters) {
+		this.clusters = clusters;
+	}
+	
+	
+	@Override
+    public String toString() {
+        return String.valueOf(clusters.keySet());
+    }
+}
