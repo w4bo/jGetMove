acdaf72683eeb06d4472e863ada597e8c02772a0
Carmona-Anthony <anthonycarmona@outlook.fr>
2017-04-16 19:19:28 +0200

    Ajout des commentaires sur le solverManager

--- src/fr/jgetmove/jgetmove/solver/SolverManager.java
+++ src/fr/jgetmove/jgetmove/solver/SolverManager.java
@@ -2,24 +2,61 @@ package fr.jgetmove.jgetmove.solver;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Set;
 
 import fr.jgetmove.jgetmove.database.Database;
 import fr.jgetmove.jgetmove.detector.Detector;
 import fr.jgetmove.jgetmove.motifs.Pattern;
 
+/**
+ * Manager that handles a clusterGenerator and detectors
+ *
+ */
 public class SolverManager {
 	
+	/**
+	 * ClusterGenerator that will handle the creation of itemsets
+	 */
 	ClusterGenerator clusterGenerator;
+	
+	/**
+	 * Detectors of patterns
+	 */
 	Set<Detector> detectors;
+	
+	/**
+	 * The database 
+	 */
 	Database database;
 	
+	/**
+	 * Constructor
+	 * @param database database from files
+	 * @param clusterGenerator ClusterGenerator that will handle the creation of itemsets
+	 * @param detectors list of detectors
+	 */
 	public SolverManager(Database database , ClusterGenerator clusterGenerator, Set<Detector> detectors){
 		this.database = database;
 		this.clusterGenerator = clusterGenerator;
 		this.detectors = detectors;
 	}
 	
+	/**
+	 * Constructor
+	 * @param database database from files
+	 * @param clusterGenerator ClusterGenerator that will handle the creation of itemsets
+	 */
+	public SolverManager(Database database , ClusterGenerator clusterGenerator){
+		this.database = database;
+		this.clusterGenerator = clusterGenerator;
+		detectors = new HashSet<Detector>();
+	}
+	
+	/**
+	 * Generate a list of clusters (Itemsets)
+	 * @return the list of clusters (Itemsets) generated from clusterGenerator
+	 */
 	public ArrayList<ArrayList<Integer>> generateClusters(){
 		return clusterGenerator.generateClusters(database);
 	}
